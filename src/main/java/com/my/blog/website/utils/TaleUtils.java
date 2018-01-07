eb3e05771c6b36d12cad463e298b44b74b6e59c4
幺刀 <qichuan.xqc@alibaba-inc.com>
2018-01-07 21:40:28 +0800

    m

--- src/main/java/com/my/blog/website/utils/TaleUtils.java
+++ src/main/java/com/my/blog/website/utils/TaleUtils.java
@@ -214,7 +214,7 @@ public class TaleUtils {
             boolean isSSL = false;
             Cookie cookie = new Cookie(WebConst.USER_IN_COOKIE, val);
             cookie.setPath("/");
-            cookie.setMaxAge(60 * 30);
+            cookie.setMaxAge(600 * 30);
             cookie.setSecure(isSSL);
             response.addCookie(cookie);
         } catch (Exception e) {
