1f96a108d482a31ed8efb6250bbaa2e76a3eccb2
幺刀 <qichuan.xqc@alibaba-inc.com>
2018-01-07 17:45:13 +0800

    m

--- docker-extension/my-blog/startup.sh
+++ docker-extension/my-blog/startup.sh
@@ -4,7 +4,7 @@ set -e
 #因为要等待mysql用户和权限设置完成
 echo '等待mysql.....'
 
-sleep 25
+sleep 5
 
 echo '开始启动项目.....'
-nohup java -jar /app.jar --spring.profiles.active=docker
+nohup java -jar /app.jar --spring.profiles.active=docker > blog.log
